2002ea78a136d56ab557eb841c6e321b1b66c989
Caroline Flores <caroline1998flores@gmail.com>
2025-04-18 01:22:38 -0300

    first commit

--- src/components/BannerText.jsx
+++ src/components/BannerText.jsx
@@ -7,13 +7,13 @@ const BannerText = () => {
       <div className="banner-overlay">
         <div className="content-wrapper">
           <div className="text-container">
-            <h1>Seja bem-vindo(a)</h1>
+            <h1>Seja Bem-Vindo(a)</h1>
             <p>
               Uma alimentação equilibrada é a chave para uma vida 
               mais saudável. Vamos juntos transformar seus hábitos
                e alcançar o seu melhor!
             </p>
-            <h2 className="subtitle">Nutricionista Eduarda Giendruczak</h2>
+            <h2>Nutricionista Eduarda Giendruczak</h2>
           </div>
         </div>
       </div>
